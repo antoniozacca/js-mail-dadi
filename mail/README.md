08339c38a75ecdad3d88b82b92d57264e429bf78
antoniozacca <antoniozaccaria057@gmail.com>
2024-05-02 15:48:14 +0200

    finish Mail

--- mail/README.md
+++ mail/README.md
@@ -1,4 +1,12 @@
 Mail
 Chiedi all’utente la sua email,
+
+
+
 controlla che sia nella lista di chi può accedere,
+
+
+
+
+
 stampa un messaggio appropriato sull’esito del controllo.
--- mail/mail.js
+++ mail/mail.js
@@ -1,1 +1,17 @@
 'use strict'
+const btnGenerate = document.getElementById("btn");
+const elencoUsers= ["antonio@gmail.com", "giuseppe@gmail.com", "ignazio@gmail.com"];
+const emailUser= document.getElementById("email");
+//click
+btnGenerate.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    //leggere email da input
+    const email = emailUser.value;
+    if (elencoUsers.includes(email)){
+        alert('Accesso eseguito');
+    } else {
+        alert('Accesso non consentito');
+    }
+
+})
