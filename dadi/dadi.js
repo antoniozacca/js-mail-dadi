cfb07bf419f39b3f0fc13c9cc005415c30fce401
antoniozacca <antoniozaccaria057@gmail.com>
2024-05-02 16:40:05 +0200

    finish mail

--- dadi/dadi.js
+++ dadi/dadi.js
@@ -1,1 +1,30 @@
 'use strict'
+const btnGenerate = document.getElementById("btn");
+const numeroComputer = document.querySelector("p#numero-computer");
+const numeroUtente = document.querySelector("p#numero-utente");
+const vincitore = document.querySelector("p#vincitore");
+
+//click
+btnGenerate.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    //genera un numero casuale computer
+    let numeroCasualeComputer = Math.floor(Math.random() * 6) + 1;
+    //portare il numero casuale in output
+    numeroComputer.innerHTML = numeroCasualeComputer;
+
+    //genera un numero casuale utente
+    let numeroCasualeUtente = Math.floor(Math.random() * 6) + 1;
+    //portare il numero casuale in output
+    numeroUtente.innerHTML = numeroCasualeUtente;
+
+    //confronto dei numeri per decretare il vincitore
+    if (numeroCasualeComputer > numeroCasualeUtente) {
+        vincitore.innerHTML = "Computer";
+    } else if (numeroCasualeComputer < numeroCasualeUtente){
+        vincitore.innerHTML = "Utente";
+    } else {
+        vincitore.innerHTML = "Pareggio";
+    }
+
+})
